a4d366684e6996028ac00c16d067bc45bdce4563
Ryan Dunn <me@ryandunn.co>
2018-07-05 08:24:44 -0500

    Updated to formally setup config file and improve repo structure, no features added.

---
+++ config-example.php
@@ -0,0 +1,18 @@
+<?php
+
+$dev_mode = false;
+
+if($dev_mode){
+    // place the auth token from your sandbox acccount here
+    $authToken = "...";
+
+    // "Action Item" tag GUID as it relates to your sandbox account here
+    $actionitem_tag_guid = array("...");
+}else{
+    // place the auth token from your live acccount here
+    $authToken = "...";
+
+    // "Action Item" tag GUID as it relates to your live account here
+    $actionitem_tag_guid = array("42cdbc60-d081-45b5-960e-1c7d0f78b066");
+}
+?>
